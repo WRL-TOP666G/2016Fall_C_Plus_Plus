0b35eff1b077a47df3e695f006df3b37b484775e
WeiRu-Bob <32428087+WeiRu-Bob@users.noreply.github.com>
2017-10-28 06:46:55 +0800

    A name card

---
+++ 1/HW1_E24039122.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "==============================================================" << endl;
+    cout << "=  安安你好!!                                                =" << endl;
+    cout << "=  I am a senior student.                                    =" << endl;
+    cout << "=  Given that I'm a transfer student,                        =" << endl;
+    cout << "=  I attend computer science class in my last college years. =" << endl;
+    cout << "=  I like to  design robots,                                 =\n";
+    cout << "=  also making them be vivid.                                =" << endl;
+    cout << "=  If you are interested in me.                              =" << endl;
+    cout << "=  Please let me know and contact me.                        =" << endl;
+    cout << "=  Email:a900240@yahoo.com.tw                                =\n";
+    cout << "==============================================================" << endl;
+    return 0;
+}
